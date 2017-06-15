1b717def7b4fed1ebcd176de0b2bedce286f0a53
leochai <chaiyiliang@qq.com>
2017-06-15 11:26:05 +0800

    20170615

--- scripts/global.js
+++ scripts/global.js
@@ -294,13 +294,75 @@ function focusLabels() {
             element.focus();
         }
     }
-} 
-
-function isFilled(field){
-    if(field.value.replace(' ', '').length==0)return false;
-    var placeholder = field.getAttribute("placeholder");
-    return(field.value!=placeholder);
 }
+
+function getHTTPObject() {
+    if (typeof XMLHttpRequest == "undefined") {
+        try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+        catch (e) { }
+        try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+        catch (e) { }
+        try { return new ActiveXObject("Msxml2.XMLHTTP"); }
+        catch (e) { }
+        return false;
+    }
+    return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element) {
+    while (element.hasChildNodes()) {
+        element.removeChild(element.lastChild);
+    }
+    var content = document.createElement("img");
+    content.setAttribute("src", "images/loading.gif");
+    content.setAttribute("alt", "Loading...");
+    element.appendChild(content);
+}
+
+function submitFormWithAjax(whichform, thetarget) {
+    //alert("here");
+    var request = getHTTPObject();
+    if (!request) { return false; }
+    displayAjaxLoading(thetarget);
+    var dataParts = [];
+    var element;
+    for (var i = 0; i < whichform.elements.length; i++) {
+        element = whichform.elements[i];
+        dataParts[i] = element.name + '=' + encodeURIComponent(element.value);
+    }
+    var data = dataParts.join('&');
+    //alert(data);
+    request.open('POST', whichform.getAttribute("action"), true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            if (request.status == 200 || request.status == 0) {
+                var matches = request.responseText.match(/<article>([\s\S]+)<\/article>/);
+                if (matches.length > 0) {
+                    thetarget.innerHTML = matches[1];
+                } else {
+                    thetarget.innerHTML = '<p>Oops, there was an error. Sorry.</p>';
+                }
+            } else {
+                thetarget.innerHTML = '<p>' + request.statusText + '</p>';
+            }
+        }
+    };
+    request.send(data);
+    return true;
+}
+
+function prepareForms() {
+    for (var i = 0; i < document.forms.length; i++) {
+        var thisform = document.forms[i];
+        thisform.onsubmit = function () {
+            var article = document.getElementsByTagName("article")[0];
+            if (submitFormWithAjax(this, article)) return false;
+            return true;
+        }
+    }
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
@@ -310,3 +372,4 @@ addLoadEvent(stripeTables);
 addLoadEvent(highlightRows);
 addLoadEvent(displayAbbreviations);
 addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
